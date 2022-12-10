67e992edc1f690193d736d9635fb8105bee5f9a4
Samuel da Silva <108034309+SamColt36@users.noreply.github.com>
2022-12-10 17:58:35 -0300

    Add files via upload

---
+++ program_3/src/aplication/Program.java
@@ -0,0 +1,33 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Student student = new Student();
+		System.out.print("Nome: ");
+		Scanner sc = new Scanner(System.in);
+		student.name = sc.nextLine();
+		System.out.print("N1: ");
+		student.nota1 = sc.nextDouble();	
+		System.out.print("N2: ");
+		student.nota2 = sc.nextDouble();
+		System.out.print("N3: ");
+		student.nota3 = sc.nextDouble();
+		sc.close();
+		student.nota_final();
+		System.out.printf("FINAL GRADE = %.2f%n", student.nota_final());
+		if (student.nota_final()>=60) {
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAILED");
+			System.out.printf("MISSING %.2f POINTS", student.restante());
+		}
+	}
+
+}
